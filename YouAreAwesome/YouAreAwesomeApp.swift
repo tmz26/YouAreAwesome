716dbac4c0b97ea564c7ec83ffbbd1b3e18d6a0a
Tomi <tamas.almasi98@gmail.com>
2025-09-30 20:26:41 +0300

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Almasi Tamás Csaba on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
