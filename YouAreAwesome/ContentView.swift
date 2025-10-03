ed21334acd9228a6b5f0d962f66d7105ae7fea34
Tomi <tamas.almasi98@gmail.com>
2025-10-03 06:07:59 +0300

    Added a  button to the first leassons project.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    @State var message = "I am a Programmer!"
+     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am a Developer")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     }
